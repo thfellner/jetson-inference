21dc9e65ba984d64040fec224b03cdc33595a0a4
Thomas Fellner <fellnertho@gmail.com>
2021-01-18 15:50:50 +0100

    update imagenet.py

--- python/examples/imagenet.py
+++ python/examples/imagenet.py
@@ -24,9 +24,11 @@
 import jetson.inference
 import jetson.utils
 
+import paho.mqtt.client as mqtt
+
 import argparse
 import sys
-
+import time
 
 # parse the command line
 parser = argparse.ArgumentParser(description="Classify a live camera stream using an image recognition DNN.", 
@@ -44,11 +46,11 @@ parser.add_argument('--headless', action='store_true', default=(), help="run wit
 is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
 
 try:
-	opt = parser.parse_known_args()[0]
+        opt = parser.parse_known_args()[0]
 except:
-	print("")
-	parser.print_help()
-	sys.exit(0)
+        print("")
+        parser.print_help()
+        sys.exit(0)
 
 
 # load the recognition network
@@ -59,33 +61,40 @@ input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
 output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
 font = jetson.utils.cudaFont()
 
+client = mqtt.Client()
+client.connect("broker.mqttdashboard.com", 1883)
+client.publish("SimulationFactory", "connected")
+
 # process frames until the user exits
 while True:
-	# capture the next image
-	img = input.Capture()
+        # capture the next image
+        img = input.Capture()
 
-	# classify the image
-	class_id, confidence = net.Classify(img)
+        # classify the image
+        class_id, confidence = net.Classify(img)
 
-	# find the object description
-	class_desc = net.GetClassDesc(class_id)
+        # find the object description
+        class_desc = net.GetClassDesc(class_id)
 
-	# overlay the result on the image	
-	font.OverlayText(img, img.width, img.height, "{:05.2f}% {:s}".format(confidence * 100, class_desc), 5, 5, font.White, font.Gray40)
-	
-	# render the image
-	output.Render(img)
+        # overlay the result on the image       
+        font.OverlayText(img, img.width, img.height, "{:05.2f}% {:s}".format(confidence * 100, class_desc), 5, 5, font.White, font.Gray40)
+        
+        # render the image
+        output.Render(img)
 
-	# update the title bar
-	output.SetStatus("{:s} | Network {:.0f} FPS".format(net.GetNetworkName(), net.GetNetworkFPS()))
-
-	# print out performance info
-	net.PrintProfilerTimes()
-
-	# exit on input/output EOS
-	if not input.IsStreaming() or not output.IsStreaming():
-		break
-
-	
+        print(confidence)
+        if confidence > 0.7:
+            client.publish("SimulationFactory", class_desc)
 
 
+        # update the title bar
+        output.SetStatus("{:s} | Network {:.0f} FPS".format(net.GetNetworkName(), net.GetNetworkFPS()))
+
+        # print out performance info
+        net.PrintProfilerTimes()
+
+        time.sleep(0.1)
+
+        # exit on input/output EOS
+        if not input.IsStreaming() or not output.IsStreaming():
+                break
